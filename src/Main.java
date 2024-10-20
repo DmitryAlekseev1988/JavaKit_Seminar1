f8baca44b6087e6fc71b87a31fda376956c6eaa1
Dmitry <88_d1m@mail.ru>
2024-10-20 09:37:25 +0300

    домашняя работа 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+// Домашняя 1
+public class Main {
+    public static void main(String[] args) {
+        ServerWindow serverWindow = new ServerWindow();
+        new ClientGUI(serverWindow);
+        new ClientGUI(serverWindow);
+    }
+}
